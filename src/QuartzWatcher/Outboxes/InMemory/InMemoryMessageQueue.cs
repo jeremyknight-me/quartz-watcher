fe87eae97abb05f8944882b30e935d76db30892b
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2025-10-05 00:56:56 -0500

    added bounded example

--- src/QuartzWatcher/Outboxes/InMemory/InMemoryMessageQueue.cs
+++ src/QuartzWatcher/Outboxes/InMemory/InMemoryMessageQueue.cs
@@ -7,6 +7,12 @@ internal sealed class InMemoryMessageQueue
     private readonly Channel<QuartzMessage> _channel
         = Channel.CreateUnbounded<QuartzMessage>();
 
+    //private readonly Channel<QuartzMessage> _channel
+    //    = Channel.CreateBounded<QuartzMessage>(new BoundedChannelOptions(10000)
+    //    {
+    //        FullMode = BoundedChannelFullMode.Wait
+    //    });
+
     public ChannelReader<QuartzMessage> Reader => _channel.Reader;
     public ChannelWriter<QuartzMessage> Writer => _channel.Writer;
 }
